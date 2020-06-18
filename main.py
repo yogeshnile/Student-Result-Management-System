8537b701d989fd35f2df1c9630fec28beed51150
Yogesh Nile <yogeshnile786@gmail.com>
2020-06-18 11:20:40 +0530

    Index Page Ready

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ import pymysql
 import os
 from datetime import datetime
 
+app = Flask(__name__)
+
+with open('config.json','r') as c:
+    params = json.load(c)["params"]
+
+app.config["SQLALCHEMY_DATABASE_URI"] = params['local_uri']
+app.secret_key = 'super-secret-key'
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def home():
+    #return "hello"
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 app.run(debug=True)
